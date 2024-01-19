3df1a2532b2f1cde3688b756ea426f4720a6c28f
ashleegaf <ashleegaf@gmail.com>
2024-01-19 16:08:09 -0500

    feat: add products api

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,24 @@
+import { promises as fs } from 'fs';
+import { ProductsType } from '@/types/products';
+
+interface IParams {
+    params: { id: string };
+}
+
+const PRODUCTS_LOCATION = 'src/server/data/products.json';
+
+export async function GET(request: Request, { params }: IParams) {
+    try {
+        const file = await fs.readFile(`${process.cwd()}/${PRODUCTS_LOCATION}`, 'utf-8');
+        if (!file) {
+            throw new Error();
+        }
+        const data: ProductsType = JSON.parse(file);
+        const selectedProduct = data.find((product) => product.id === params.id);
+        return Response.json({ data: selectedProduct }, { status: 201 });
+    } catch (error) {
+        return new Response(`Error retrieving products.`, {
+            status: 404,
+        });
+    }
+}
