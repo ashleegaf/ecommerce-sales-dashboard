81d41cdc5db3327a4d780a49681ac7d91fbe7152
ashleegaf <ashleegaf@gmail.com>
2024-01-19 17:05:03 -0500

    feat: wrap providers component around app

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -3,6 +3,8 @@
 import { Provider } from 'react-redux';
 import { store } from '@/lib/redux/store';
 
-export const Providers = (props: React.PropsWithChildren) => {
+const Providers = (props: React.PropsWithChildren) => {
     return <Provider store={store}>{props.children}</Provider>;
 };
+
+export default Providers;
